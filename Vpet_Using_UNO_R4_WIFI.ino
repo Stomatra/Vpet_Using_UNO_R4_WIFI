5ed74929af93e639398bfc4b24f24fb210067b9d
Stoma <3223572901@qq.com>
2025-11-26 13:30:04 +0800

    添加项目文件。

---
+++ Vpet_Using_UNO_R4_WIFI.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Vpet_Using_UNO_R4_WIFI.ino
+ Created:	2025/11/26 13:26:58
+ Author:	yjh12
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
